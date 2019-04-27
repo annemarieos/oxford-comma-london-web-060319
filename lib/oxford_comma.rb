6f57fb8ddf9673d65b5a1ff6383ce038e0f3cd10
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-27 13:27:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-if array.length == 2
-  return "#{array[0] and #{array[1]}"
-elsif 2 > array.lengthsarray[-1].insert(0, "and ")
-end
+  if array.length == 2
+    return "#{array[0] and #{array[1]}"
+  elsif 2 > array.lengthsarray[-1].insert(0, "and ")
+  end
+  array.join(", ")
+end    
